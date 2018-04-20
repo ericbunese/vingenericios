66b8d775b475b202dfafccf951ff7c9f7b43b5f8
Eric Bunese <ericbunese@gmail.com>
2018-04-20 14:36:48 -0300

    First Version

--- vingenericIos/ViewController.swift
+++ vingenericIos/ViewController.swift
@@ -8,11 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
+
+  //MARK: Objects
+  
+  @IBOutlet weak var offsetLabel: UILabel!
+  @IBOutlet weak var offsetLabel2: UILabel!
+  @IBOutlet weak var offsetStepper: UIStepper!
+  @IBOutlet weak var offsetStepper2: UIStepper!
+  @IBOutlet weak var textFieldChave: UITextField!
+  @IBOutlet weak var textFieldClaro: UITextField!
+  @IBOutlet weak var textFieldCifrado: UITextField!
+  
+  let ving = Vingenere()
+  var prod = 3
+  var keyboardHeight: CGFloat = 0.0
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    self.textFieldChave.delegate = self
+    self.textFieldClaro.delegate = self
+    self.textFieldCifrado.delegate = self
+    
+    self.offsetStepper.maximumValue = Double(self.ving.const.count)
+    self.offsetStepper2.maximumValue = Double(self.ving.const.count)
+    
+    self.offsetLabel.text = "Offset: \(Int(self.offsetStepper.value))"
+    self.offsetLabel2.text = "Offset: \(Int(self.offsetStepper2.value))"
+    self.prod = Int(self.offsetStepper2.value)*Int(self.offsetStepper.value)
+    
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(keyboardWillShow),
+      name: NSNotification.Name.UIKeyboardWillShow,
+      object: nil
+    )
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +51,71 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
+  //MARK: Actions
+  @IBAction func pressedCifrar(_ sender: Any) {
+    self.textFieldCifrado.text = self.ving.encode(prod: self.prod)
+  }
+  
+  @IBAction func actionDecifrar(_ sender: Any) {
+    self.textFieldClaro.text = self.ving.decode(prod: self.prod)
+  }
+  
+  //MARK: UITextFieldDelegate
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    self.animateViewMoving(moveValue: CGFloat.minimum(-(textField.frame.minY-self.keyboardHeight), 0.0))
+  }
+  
+  @IBAction func offsetStepperChanged(_ sender: Any) {
+    self.offsetLabel.text = "Offset: \(Int(self.offsetStepper.value))"
+    self.prod = Int(self.offsetStepper2.value)*Int(self.offsetStepper.value)
+  }
+  
+  @IBAction func offsetStepper2Changed(_ sender: Any) {
+    self.offsetLabel2.text = "Offset: \(Int(self.offsetStepper2.value))"
+    self.prod = Int(self.offsetStepper2.value)*Int(self.offsetStepper.value)
+  }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // User finished typing (hit return): hide the keyboard.
+    textField.resignFirstResponder()
+    self.animateViewMoving(moveValue: nil)
+    
+    if textField == self.textFieldChave {
+      self.ving.key = textField.text!
+    }
+    else if textField == self.textFieldClaro {
+      self.ving.text = textField.text!
+    }
+    else if textField == self.textFieldCifrado {
+      self.ving.cypher = textField.text!
+    }
+    return true
+  }
+  
+  func animateViewMoving (moveValue :CGFloat?){
+    var center: CGPoint
+    if moveValue != nil {
+      center = CGPoint(x: 0.0, y: moveValue!)
+    }
+    else {
+      center = CGPoint(x: 0.0, y: 0.0)
+    }
+    let size = CGSize(width: view.frame.size.width, height: view.frame.size.height)
+    let resetFrame = CGRect(origin: center, size: size)
+    let movementDuration:TimeInterval = 0.3
+    //let movement:CGFloat = moveValue
+    UIView.beginAnimations( "animateView", context: nil)
+    UIView.setAnimationBeginsFromCurrentState(true)
+    UIView.setAnimationDuration(movementDuration )
+    self.view.frame = resetFrame
+    UIView.commitAnimations()
+  }
+  
+  @objc func keyboardWillShow(_ notification: Notification) {
+    if let keyboardFrame: NSValue = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+        let keyboardRectangle = keyboardFrame.cgRectValue
+        self.keyboardHeight = keyboardRectangle.height
+    }
+}
 }
 
---
+++ vingenericIos/vingenere.swift
@@ -0,0 +1,96 @@
+//
+//  vingenere.swift
+//  vingeneric
+//
+//  Created by Eric Bunese on 18/04/2018.
+//  Copyright © 2018 Eric Bunese. All rights reserved.
+//
+
+import Foundation
+
+class Vingenere {
+  var text: String = ""
+  var key: String = ""
+  var cypher: String = ""
+  
+  let const = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6, 4, 3, 3, 8, 3, 2, 7, 9, 5, 0, 2, 8, 8, 4, 1, 9, 7, 1, 6, 9, 3, 9, 9, 3, 7, 5, 1, 0, 5, 8, 2, 0, 9, 7, 4, 9, 4, 4, 5, 9, 2, 3, 0, 7, 8, 1, 6, 4, 0, 6, 2, 8, 6, 2, 0, 8, 9, 9, 8, 6, 2, 8, 0, 3, 4, 8, 2, 5, 3, 4, 2, 1, 1, 7, 0, 6, 7, 9, 8, 2, 1, 4, 8, 0, 8, 6, 5, 1, 3, 2, 8, 2, 3, 0, 6, 6, 4, 7, 0, 9, 3, 8, 4, 4, 6, 0, 9, 5, 5, 0, 5, 8, 2, 2, 3, 1, 7, 2, 5, 3, 5, 9, 4, 0, 8, 1, 2, 8, 4, 8, 1, 1, 1, 7, 4, 5, 0, 2, 8, 4, 1, 0, 2, 7, 0, 1, 9, 3, 8, 5, 2, 1, 1, 0, 5, 5, 5, 9, 6, 4, 4, 6, 2, 2, 9, 4, 8, 9, 5, 4, 9, 3, 0, 3, 8, 1, 9, 6, 4, 4, 2, 8, 8, 1, 0, 9, 7, 5, 6, 6, 5, 9, 3, 3, 4, 4, 6, 1, 2, 8, 4, 7, 5, 6, 4, 8, 2, 3, 3, 7, 8, 6, 7, 8, 3, 1, 6, 5, 2, 7, 1, 2, 0, 1, 9, 0, 9, 1]
+  private var alphabet: [String: [String]] = [:]
+  
+  init(){
+    self.initAlphabet()
+  }
+  
+  func encode(prod: Int) -> String{
+    var str: String = ""
+    var pos: Int = 0
+    let igl = self.alphabet[""]
+    
+    for c in self.text {
+      let k = self.key[self.key.index(self.key.startIndex, offsetBy: (pos % self.key.count))]
+      if let alf = self.alphabet["\(k)"]{
+        let off = self.const[(prod+pos) % self.key.count]
+        if let chr = alf.index(of: "\(c)"){
+          str += igl![(chr+off) % (igl?.count)!]
+        }
+      }
+      pos = pos+1
+    }
+    
+    self.cypher = str
+    return str
+  }
+  
+  func decode(prod: Int) -> String {
+    var str: String = ""
+    var pos: Int = 0
+    let igl = self.alphabet[""]
+    
+    for c in self.cypher{
+      let k = self.key[self.key.index(self.key.startIndex, offsetBy: (pos % self.key.count))]
+      if let alf = self.alphabet["\(k)"]{
+        let off = self.const[(prod+pos) % self.key.count]
+        let chr = igl?.index(of: "\(c)")
+        str += alf[self.modulo(val: (chr!-off), low: 0, upp: alf.count)]
+      }
+      pos = pos+1
+    }
+    
+    return str
+  }
+  
+  private func modulo(val: Int, low: Int, upp: Int) -> Int{
+    var v: Double = 0.0
+    let o = Double(low)
+    let d = Double(val)-Double(o)
+    let w = Double(upp)-Double(o)
+  
+    v = d-floor(d/w)*w+o
+  
+    return Int(v)
+  }
+  
+  private func initAlphabet(){
+    var alf = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Ã", "Â", "Á", "À", "Ê", "É", "È", "Î", "Í", "Ì", "Õ", "Ô", "Ó", "Ò", "Û", "Ú", "Ù", ".", ",", ":", ";", "!", "?", "@", "#", "$", "%", "&", "*", "(", ")", "-", "+", "=", "_", "/", "\\", "\"", "´", "`", "ˆ", "~", "[", "{", "]", "}", "<", ">", "ã", "â", "á", "à", "ê", "é", "è", "î", "í", "ì", "õ", "ô", "ó", "ò", "û", "ú", "ù", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ç", "Ç"]
+    let emojiRanges = [
+      0x1F601...0x1F64F,
+      0x2702...0x27B0,
+      0x1F680...0x1F6C0,
+      0x1F170...0x1F251
+    ]
+
+    for range in emojiRanges {
+        for i in range {
+          let c = String(UnicodeScalar(i)!)
+            alf.append(c)
+        }
+    }
+    let igl = alf
+    self.alphabet[""] = igl
+    
+    for c in igl{
+      let pop = alf.remove(at: 0)
+      alf.append(pop)
+      self.alphabet["\(c)"] = alf
+    }
+  }
+}
